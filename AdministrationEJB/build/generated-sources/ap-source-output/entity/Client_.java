692ed27f04d145f40091359102dacc91834615aa
Nemolovich <nemolovich@hotmail.fr>
2013-06-24 18:10:54 +0200

    Test from NetBeans

--- AdministrationEJB/build/generated-sources/ap-source-output/entity/Client_.java
+++ AdministrationEJB/build/generated-sources/ap-source-output/entity/Client_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-06-24T17:58:37")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-06-24T18:09:52")
 @StaticMetamodel(Client.class)
 public class Client_ { 
 
